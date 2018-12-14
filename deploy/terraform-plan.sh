0151738c0a3e641b13d993f898e24ebbb58b38ba
Liam Reilly <liam.reilly.1@gmail.com>
2018-12-14 01:05:00 +0000

    Updated config

--- deploy/terraform-plan.sh
+++ deploy/terraform-plan.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-echo $AWS_ACCESS_KEY_ID
+KEY='aws_access_key=${AWS_ACCESS_KEY_ID}'
+SECRET='aws_secret_key=${AWS_SECRET_KEY}'
 
-terraform plan -input=false -out=terraform.plan -var 'aws_access_key=$AWS_ACCESS_KEY_ID' -var 'aws_secret_key=$AWS_SECRET_KEY'
+echo ${KEY}
+
+terraform plan -input=false -out=terraform.plan -var ${KEY} -var ${SECRET}
