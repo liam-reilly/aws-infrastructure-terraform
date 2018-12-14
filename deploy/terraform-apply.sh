d649ce1614ed6381add0182fd57ea6f92d894fd5
Liam Reilly <liam.reilly.1@gmail.com>
2018-12-14 01:31:49 +0000

    Tweaked terraform-apply.sh

--- deploy/terraform-apply.sh
+++ deploy/terraform-apply.sh
@@ -3,4 +3,4 @@
 KEY="aws_access_key=$AWS_ACCESS_KEY_ID"
 SECRET="aws_secret_key=${AWS_SECRET_KEY}"
 
-terraform apply terraform.plan -input=false -var ${KEY} -var ${SECRET}
+terraform apply -input=false -var ${KEY} -var ${SECRET} ./terraform.plan 
