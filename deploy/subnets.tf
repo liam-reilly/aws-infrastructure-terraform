dc546d2d0dbda1d581fd1821cea01dfc436701f1
Liam Reilly <liam.reilly.1@gmail.com>
2019-01-08 18:09:51 +0000

    Creating 9x subnets

--- deploy/subnets.tf
+++ deploy/subnets.tf
@@ -1,35 +1,35 @@
-//resource "aws_subnet" "public-subnet" {
-//  count             = 3
-//  vpc_id            = "${aws_vpc.private.id}"
-//  cidr_block        = "${element(var.public_cidr, count.index)}"
-//  availability_zone = "${element(var.azs, count.index)}"
-//
-//  tags {
-//    Name = "public.${var.region}.az${count.index + 1}"
-//    Tier = "public"
-//  }
-//}
-//
-//resource "aws_subnet" "private-subnet" {
-//  count             = 3
-//  vpc_id            = "${aws_vpc.private.id}"
-//  cidr_block        = "${element(var.private_cidr, count.index)}"
-//  availability_zone = "${element(var.azs, count.index)}"
-//
-//  tags {
-//    Name = "private.${var.region}.az${count.index + 1}"
-//    Tier = "private"
-//  }
-//}
-//
-//resource "aws_subnet" "data-subnet" {
-//  count             = 3
-//  vpc_id            = "${aws_vpc.private.id}"
-//  cidr_block        = "${element(var.data_cidr, count.index)}"
-//  availability_zone = "${element(var.azs, count.index)}"
-//
-//  tags {
-//    Name = "data.${var.region}.az${count.index + 1}"
-//    Tier = "data"
-//  }
-//}
+resource "aws_subnet" "public-subnet" {
+  count             = 3
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${element(var.public_cidr, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags {
+    Name = "public.${var.region}.az${count.index + 1}"
+    Tier = "public"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  count             = 3
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${element(var.private_cidr, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags {
+    Name = "private.${var.region}.az${count.index + 1}"
+    Tier = "private"
+  }
+}
+
+resource "aws_subnet" "data-subnet" {
+  count             = 3
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${element(var.data_cidr, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags {
+    Name = "data.${var.region}.az${count.index + 1}"
+    Tier = "data"
+  }
+}
