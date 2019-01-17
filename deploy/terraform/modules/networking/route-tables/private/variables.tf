156f0dc29073877eee437e199063e1fb8dec5907
Liam Reilly <liam.reilly.1@gmail.com>
2019-01-17 00:27:34 +0000

    Fixed type of nat_gateway_ids var

--- deploy/terraform/modules/networking/route-tables/private/variables.tf
+++ deploy/terraform/modules/networking/route-tables/private/variables.tf
@@ -1,13 +1,19 @@
 variable "aws_region" {}
 variable "vpc_id" {}
 variable "vpc_cidr_block" {}
+
 variable "availability_zones" {
   type = "list"
 }
+
 variable "public_subnet_ids" {
   type = "list"
 }
+
 variable "private_subnet_ids" {
   type = "list"
 }
-variable "nat_gateway_ids" {}
+
+variable "nat_gateway_ids" {
+  type = "list"
+}
